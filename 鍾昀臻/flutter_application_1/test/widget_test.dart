d06213874027663aa8090eba851dfccc8ce9a6d7
Chungyunzhenz <128110617+Chungyunzhenz@users.noreply.github.com>
2024-05-03 23:47:31 +0800

    updata

--- 鍾昀臻/flutter_application_1/test/widget_test.dart
+++ 鍾昀臻/flutter_application_1/test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import "package:flutter/material.dart";
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_application_1/main.dart';
+import 'package:package_fonts/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
